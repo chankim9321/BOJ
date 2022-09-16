5692d76481bfc26867e10873888c1da8bc9b92bc
byoungchan <byoungchan9321@gmail.com>
2022-09-16 17:40:26 +0900

    9184.cpp 속도 테스트 수정

--- cpp/9184.cpp
+++ cpp/9184.cpp
@@ -7,26 +7,15 @@ using namespace std;
 int matrix[21][21][21];
 
 int w(int a, int b, int c){
-	if(a <= 0 OR b <=0 OR c <= 0) return 1;
-	if(a > 20 OR b > 20 OR c > 20) return w(20,20,20);
-	if(a < b AND b < c ) {
-		if(matrix[a][b][c] != 0){
-			return matrix[a][b][c];
-		}
-		else {
-			matrix[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
-			return matrix[a][b][c];
-		}
+	if(a <= 0 OR b <=0 OR c <= 0) return 1; // 하나라도 0 이하라면 1 반환
+	if(a > 20 OR b > 20 OR c > 20) return w(20,20,20); // 하나라도 20을 초과하는 파라미터라면 20으로 변환하여 호출
+	if(matrix[a][b][c] != 0){
+		return matrix[a][b][c];
 	}
-	else {
-		if(matrix[a][b][c] != 0){
-			return matrix[a][b][c];
-		}
-		else {
-			matrix[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c) + w(a-1,b,c-1) - w(a-1,b-1,c-1);
-			return matrix[a][b][c];
-		}
+	if(a<b AND b<c){
+		return matrix[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
 	}
+	return matrix[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c) + w(a-1,b,c-1) - w(a-1,b-1,c-1);
 }
 int main(int argc, char* argv[]){
 	int a, b, c;
