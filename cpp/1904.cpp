a1754bf0ba203b73ad8095fb7ec24e5db47bca1b
byoungchan <byoungchan9321@gmail.com>
2022-09-21 17:39:29 +0900

    1904.cpp 과도한 스택으로 dump발생, for 연산 만듬.

--- cpp/1904.cpp
+++ cpp/1904.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#define MAX 1000000 
+#define MAX 1000001 
+#define REMAINDER 15746
 int cache[MAX] = {0,};
 
-/*
-int facto(int n){
-	int res=1;
-	for(int i=n; i>0; i--){
-		res = res*i;
-	}
-	return res;
-}
-int comb(int n, int r){
-	if(r == 0) return 1;
-	if(r == 1) return n;
-	return facto(n) / (facto(n-r)*facto(r));
-}
-*/
-
 // A recurrence fomula is "D[n] = D[n-1] + D[n-2]"
-int solution(int n){
+long long solution(int n){
 	if(n==1) return 1; // D[1] = 1
 	if(n==2) return 2; // D[2] = 2
 
 	if(cache[n] == 0){
-		return cache[n] = solution(n-1) + solution(n-2);
+		return cache[n] = (solution(n-1) + solution(n-2)) % REMAINDER;
 	}
 	else{
 		return cache[n];
 	}
 }
+long long solution_2(int n){
+	cache[1]=1;
+	cache[2]=2;
+	for(int i=3; i<=n; i++){
+		cache[i] = (cache[i-1] + cache[i-2]) % REMAINDER;
+	}
+	return cache[n];
+}
 int main(int argc, char* argv[]){
+	//set fast IO
+	ios_base::sync_with_stdio(false); 
+	cin.tie(nullptr);
+
 	int size;
 	cin >> size;
-	cout << solution(size) << "\n";
+	cout << solution_2(size) << "\n";
 	return 0;
 }
