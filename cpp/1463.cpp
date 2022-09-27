a16adc7245d059310337393baa7109db51c79c58
byoungchan <byoungchan9321@gmail.com>
2022-09-27 13:23:23 +0900

    1463.cpp DP사용 풀이

--- cpp/1463.cpp
+++ cpp/1463.cpp
@@ -2,26 +2,29 @@
 #define AND &&
 #define OR ||
 using namespace std;
-int callNumber=0;
-
 int solution(int number){
-	if(number == 1) return callNumber;
-	if( (number -1) % 3 == 0) {
-		callNumber++;
-		return solution(number-1);
-	}
-	else if(number % 3 == 0){
-		callNumber++;
-		return solution(number/3);
-	}
-	else if(number % 2 == 0){
-		callNumber++;
-		return solution(number/2);
-	}
-	else{
-		callNumber++;
-		return solution(number-1);
-	}
+	int callNumber=0;
+	do{
+		if(number % 2 != 0 AND number % 3 != 0){
+			number--;
+			callNumber++;
+		}
+		else if(number % 3 != 0){
+			if( (number-1)/3 < (number/2)-1){  // D(n+2)의 결과 비교
+				number--;
+				callNumber++;
+			}
+			else{
+				number/=2;
+				callNumber++;
+			}
+		}
+		else{
+			number/=3;
+			callNumber++;
+		}
+	}while(number != 1);
+	return callNumber;
 }
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false);
