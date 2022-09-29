b6a516ef8d68de75bc266e770a8889a7e3c7c24e
byoungchan <byoungchan9321@gmail.com>
2022-09-29 21:12:20 +0900

    1654.cpp 수정

--- cpp/1654.cpp
+++ cpp/1654.cpp
@@ -8,17 +8,24 @@ int solution(int arr[], int needs, int wrans, int max){
 	int result;
 	int mid;
 	int slices;
-	while(!(result == needs)){
+	int answer;
+	while(start <= end){
 		result = 0;
 		mid = (start + end)/2;
+		if(mid == 0){
+			mid++;
+		}
 		for(int i=0; i<wrans; i++){
 			slices = arr[i] / mid;
 			result += slices;	
 		}
-		if(result > needs) start = mid;
-		else if(result < needs) end = mid;
+		if(result >= needs) { // 조건을 만족하고도 남는다면
+			start = mid+1;
+			answer = mid;
+		}
+		else end = mid-1;
 	}
-	return mid;
+	return answer;
 }
 int main(int argc, char* argv[]){
 	int wrans;
@@ -26,7 +33,7 @@ int main(int argc, char* argv[]){
 
 	cin >> wrans >> needs;
 	int *arr = new int[wrans];
-	int max=0;
+	int max = 0;
 	for(int i=0; i<wrans; i++){
 		cin >> arr[i];
 		if(arr[i] > max){
