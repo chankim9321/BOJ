23bf5e9af3b1a9e881b48289970b5f71ba242b42
byoungchan <byoungchan9321@gmail.com>
2022-10-07 00:25:38 +0900

    1946.cpp 시간복잡도 O(n)시간안에 해결

--- cpp/1946.cpp
+++ cpp/1946.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
+#include <algorithm>
+#define DEBUG_MODE 0
 using namespace std;
+
 class Human{
 	public:
 		int document;
 		int interview;
 };
 int getSmaller(int a, int b){ return a > b ? b : a; }
-int solution(Human arr[],int candidates){
+bool cmp(Human a, Human b){	return a.document < b.document;}
+
+int solution(Human arr[],int candidates,int interview_min, int document_min){
 	int max_pass=0;
 	for(int i=0; i<candidates; i++){
 		int top_grade = getSmaller(arr[i].document, arr[i].interview); 	
 		if(top_grade == arr[i].document){ // 서류전형이 높은 등수일 때
-			bool is_passed = true;
-			for(int k=0; k<candidates; k++){
-				if(arr[k].document < top_grade){
-					if(arr[k].interview > arr[i].interview) continue;
-					else{
-						is_passed = false;
-						break;
-					}
-				}
+			if(top_grade == 1){ // 1등이면 무조건 통과
+				max_pass++;
 			}
-			if(is_passed) max_pass++;
+			else if(interview_min > arr[i].interview){ // 면접전형이 최소보다 작으면 통과
+				max_pass++;
+				interview_min = arr[i].interview; // 통과 최소점수 업데이트
+			}
 		}
 		else { // 면접전형이 높은 등수일 때
-			bool is_passed = true;
-			for(int k=0; k<candidates; k++){
-				if(arr[k].interview < top_grade){ // 자기자신보다 높은 사람의 성적과 비교
-					if(arr[k].document > arr[i].document) continue; // 서류전형이 자기보다 낮다면 반복문 계속 진행
-					else{
-						is_passed = false;
-						break; // 서류전형이 자기보다 높다면 탈락
-					}
-				}
+			if(top_grade == 1){ // 1등이면 무조건 통과
+				max_pass++;
 			}
-			if(is_passed) max_pass++;
+			else if(document_min > arr[i].document){
+				max_pass++;
+				document_min = arr[i].document;
+			}
 		}
 	}
 	return max_pass;
@@ -46,14 +43,30 @@ int main(int argc, char* argv[]){
 	int testCase;
 	int result[testCase];
 	int candidates;
+	int document_min=9999999;
+	int interview_min=9999999;
 	cin >> testCase;
 	for(int i=0; i<testCase; i++){
 		cin >> candidates;
 		Human* arr = new Human[candidates];	
-		for(int j=0; j<candidates; j++){
+		for(int j=0; j<candidates; j++){ // init
 			cin >> arr[j].document >> arr[j].interview;	
+			if(arr[j].document == 1){
+				interview_min = arr[j].interview;
+			}
+			if(arr[j].interview == 1){
+				document_min = arr[j].document;
+			}
 		}
-		cout << solution(arr, candidates) << '\n';
+		sort(arr, arr+candidates,cmp);
+		#if DEBUG_MODE
+		cout << "--------------SORT-------------" << '\n';
+		for(int k=0; k<candidates; k++){
+			cout << arr[k].document << " " << arr[k].interview << '\n';
+		}
+		cout << "-------------------------------" << '\n';
+		#endif
+		cout << solution(arr, candidates, interview_min, document_min) << '\n';
 		delete[] arr;
 	}
 	return 0;
