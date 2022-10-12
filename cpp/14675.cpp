1bec676638bf1959290379a5d7904c815e64af06
byoungchan <byoungchan9321@gmail.com>
2022-10-12 17:56:01 +0900

    14675.cpp 간단 풀이

--- cpp/14675.cpp
+++ cpp/14675.cpp
@@ -8,32 +8,6 @@ class Node{
 		int id;
 		vector<Node*> link;	
 };
-int bfs(int removed, Node* Tree, int vertex){
-	int is_visited[vertex+1];
-	fill_n(is_visited, vertex+1, 0);
-	is_visited[removed] = 1;
-	int cnt=0;
-	for(int i=1; i<=vertex; i++){
-		if(!is_visited[i]){
-			queue<Node*> q;
-			q.push(&Tree[i]);
-			is_visited[i] = 1;
-			while(!q.empty()){
-				Node* root = q.front();
-				q.pop();
-				for(int i=0; i<root->link.size(); i++){
-					int idx = root->link[i]->id;
-					if(!is_visited[idx]){ // 다음 방문할 정점의 번호가 단절 대상이라면 방문 x
-						is_visited[idx] = 1;
-						q.push(root->link[i]);
-					}
-				}
-			}
-			cnt++;
-		}
-	}
-	return cnt;
-}
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
@@ -47,6 +21,7 @@ int main(int argc, char* argv[]){
 		int from, to;
 		cin >> from >> to;
 		Tree[from].link.push_back(&Tree[to]);	
+		Tree[to].link.push_back(&Tree[from]);
 	}
 	Tree[vertex].id = vertex;
 	// init end
@@ -54,14 +29,9 @@ int main(int argc, char* argv[]){
 	for(int i=0; i<query; i++){
 		int q, removed;
 		cin >> q >> removed;
-		if(q == 1){ // 단절점인지에 대한 질의
-			int graphs = bfs(removed, Tree, vertex);	
-			if(graphs > 1){
-				cout << "yes" << '\n';
-			}
-			else{
-				cout << "no" << '\n';
-			}
+		if(q == 1){ // 단절점인지에 대한 질의, 노드의 링크가 하나밖에 없으면 단절점이 아니다.
+			if(Tree[removed].link.size() == 1) cout << "no" << '\n';
+			else cout << "yes" << '\n';
 		}
 		else{ // 단절선인지에 대한 질의
 			// 정점이 무조건 2개 이상이므로 어느 간선을 자르던간에 단절선이다.
