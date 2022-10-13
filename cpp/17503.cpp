3df977c38880a6f8b8a272414c28146480b4aeb1
byoungchan <byoungchan9321@gmail.com>
2022-10-13 21:26:19 +0900

    17503.cpp 자료형 범위 초과문제 해결

--- cpp/17503.cpp
+++ cpp/17503.cpp
@@ -5,33 +5,32 @@
 #define DEBUG_MODE 1
 using namespace std;
 
-int N, M, K;
-int solution(vector<pair<int, int> >&v){
+int N, K;
+long long M;
+int solution(vector<pair<long long, long long> >&v){
 	int start = 0; // 시작인덱스
 	int end = v.size()-1; // 끝 인덱스 
-	int lastPreferCount;
-	int preferCount=0;
-	int degreeLevel=0;
+	long long lastPreferCount;
+	long long preferCount=0;
+	long long degreeLevel=5000000000;
 	// 현재 벡터는 도수레벨을 기준으로 오름차순 정렬되어 있음.
 	while(start <= end){
 		int mid = (start + end)/2; // 중간 인덱스 ex) 2
-		int level=-1;
-		cout << "산정된 인덱스값 = " << mid <<  ", 해당 인덱스값에 존재하는 도수레벨 = " << v[mid].second << '\n';
+	//	cout << "산정된 인덱스값 = " << mid <<  ", 해당 인덱스값에 존재하는 도수레벨 = " << v[mid].second << '\n';
 		if(mid < N-1){ // 선정된 인덱스값이 마셔야 되는 술보다 작을경우
-			cout << N << "잔을 마실 수 없으므로 도수레벨을 좀 더 높입니다." << '\n';
+		//	cout << N << "잔을 마실 수 없으므로 도수레벨을 좀 더 높입니다." << '\n';
 			start= mid+1;		
 			continue;
 		}
-		priority_queue<pair<int, int> > max_heap;
+		priority_queue<pair<long long, long long> > max_heap;
 		for(int i=0; i<=mid; i++){ 
 			max_heap.push(v[i]); // 선정된 인덱스 아래로 모든 데이터를 삽입
 		}
 		for(int i=0; i<N; i++){ // N개 만큼 선호도가 높은 순서대로 꺼낸다.
 			preferCount += max_heap.top().first;
-			
 			max_heap.pop();
 		}
-		cout << "산정된 선호도의 합 = " << preferCount << ", 도수레벨 : " << v[mid].second << '\n';
+		//cout << "산정된 선호도의 합 = " << preferCount << ", 도수레벨 : " << v[mid].second << '\n';
 		if(preferCount >= M){ // 선호도의 합이 요구하는 총합 선호도보다 높을 때는 레벨을 줄일 필요가 있다.
 			end = mid-1;
 			degreeLevel = v[mid].second; // 현재 도수레벨 기록
@@ -47,7 +46,7 @@ int solution(vector<pair<int, int> >&v){
 	}
 	return degreeLevel;
 }
-bool sortByDegree(pair<int, int> a, pair<int, int> b){
+bool sortByDegree(pair<long, long> a, pair<long, long> b){
 	return a.second < b.second;
 }
 int main(int argc, char* argv[]){
@@ -55,12 +54,12 @@ int main(int argc, char* argv[]){
 	cin.tie(nullptr);
 	// N = 축제가 열리는 기간, M = 채워야 하는 선호도의 합, K = 맥주 종류의 수
 	cin >> N >> M >> K; 
-	vector<pair<int, int> > beer_info;
+	vector<pair<long long, long long> > beer_info;
 
 	// 첫번째 요소는 맥주의 선호도, 두번째 요소는 도수 레벨
 	// 맥주의 정보를 입력받는다.
 	for(int i=0; i<K; i++){
-		pair<int, int> beer;
+		pair<long long, long long> beer;
 		cin >> beer.first >> beer.second;
 		beer_info.push_back(beer);
 	}
@@ -70,6 +69,6 @@ int main(int argc, char* argv[]){
 		cout << "선호도 = " << beer_info[i].first << "  도수 레벨 = " << beer_info[i].second << '\n';
 	}
 	#endif
-	cout << "정답: " << solution(beer_info) << '\n';
+	cout << solution(beer_info) << '\n';
 	return 0;
 }
