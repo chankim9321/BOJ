12d4d998de46328c307a044a23be7463d8ef1e8b
byoungchan <byoungchan9321@gmail.com>
2022-10-20 16:38:33 +0900

    2156.cpp 최소단위의 상황에서 처리

--- cpp/2156.cpp
+++ cpp/2156.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	int size;
+	int opt_max=0;
 	cin >> size;
 	int *dp = new int[size+1];
 	fill(dp, dp+size+1, 0);
@@ -16,11 +17,16 @@ int main(int argc, char* argv[]){
 		cin >> wine;
 		drinks.push_back(wine);
 	}
-	dp[1] = drinks[1];
-	dp[2] = dp[1] + drinks[2];
-	dp[3] =	max(dp[1] + drinks[3], drinks[2] + drinks[3]);
-	int opt_max = dp[3];
-	for(int i=4; i<=size; i++){
+	if(size < 2){
+		dp[1] = drinks[1];
+		opt_max = dp[1];
+	}
+	else{
+		dp[1] = drinks[1];
+		dp[2] = dp[1] + drinks[2];
+		opt_max = dp[2];
+	}
+	for(int i=3; i<=size; i++){
 		dp[i] =	max(dp[i-2] + drinks[i], dp[i-3] + drinks[i-1] + drinks[i]);
 		opt_max = max(opt_max, dp[i]);
 	}
