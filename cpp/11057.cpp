524cf6c71c5e23d7b6dd2966af86728acf852dc3
byoungchan <byoungchan9321@gmail.com>
2022-10-26 01:59:02 +0900

    11057.cpp 모듈러 연산 재수정 및 최적화

--- cpp/11057.cpp
+++ cpp/11057.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <algorithm>
+#define MOD 10007
 using namespace std;
 
 int main(int argc, char* argv[]){
 	int size;
 	cin >> size;
-	int *dp = new int(size+1);
-	fill(dp,dp+size+1,0);	
 	int memory[1001][10] = {0,};
-	dp[1] = 10;
 	for(int i=0; i<10; i++){
 		memory[1][i] = 1;
 	}
-
 	for(int i=2; i<=size; i++){
-		memory[i][0] = dp[i-1];
-		dp[i] += memory[i][0];
+		memory[i][0] = 1;
 		for(int j=1; j<10; j++){
-			memory[i][j] = memory[i][j-1] - memory[i-1][j-1];
-			dp[i] += memory[i][j];
-			dp[i] %= 10007;
+			memory[i][j] = memory[i][j-1] + memory[i-1][j];
+			memory[i][j] %= MOD;
 		}
 	}
-	cout << dp[size] << '\n';
+	int res=0;
+	for(int i=0; i<10; i++){
+		res += memory[size][i];
+	}
+	cout <<  res % MOD << '\n';
 	return 0;
 }
 
