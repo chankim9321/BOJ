8b7aa4562a7c0ffac293e9c0b1958c0c4594b01f
byoungchan <byoungchan9321@gmail.com>
2022-10-25 18:03:38 +0900

    1303.cpp bfs 함수 생성후 풀이

--- cpp/1303.cpp
+++ cpp/1303.cpp
@@ -1,77 +1,77 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <queue>
 #include <cmath>
+#include <queue>
 #define DEBUG_MODE 1
 
 using namespace std;
 
-int visited[100][100] = {0,};
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+int visited[103][103] = {0,};
 vector<string> field;
 
+int bfs(int x, int y){ 
+	int result=1;
+	queue<pair<int, int> > q;
+	visited[x][y] = 1;
+	char team = field[x][y];
+	q.push(make_pair(x,y));
+	while(!q.empty()){
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		for(int i=0; i<4; i++){
+			int X = x + dx[i];
+			int Y = y + dy[i];	
+			if(!visited[X][Y] && field[X][Y] == team){
+				visited[X][Y] = 1;
+				q.push(make_pair(X, Y));
+				result++;
+			}
+		}
+	}
+	return result*result;
+}
 pair<int, int> solution(int width, int height){
-	pair<int, int> graph;
 	pair<int, int> result;
 	result.first = 0;
 	result.second = 0;
-	graph.first = 1;
-	graph.second = 1;
-	visited[0][0] = 1;
-	for(int i=0; i<height; i++){
-		for(int j=0; j<width; j++){
-			char pos = field[i][j];
-			int flag = 0;
-			if(pos == field[i+1][j] && !visited[i+1][j]){ // SOUTH
-				visited[i+1][j] = 1;
-				flag = 1;
-				if(pos == 'W') graph.first++;
-				else graph.second++;
-			}
-			if(pos == field[i-1][j] && !visited[i-1][j]){ // NORTH
-				visited[i-1][j] = 1;
-				flag = 1;
-				if(pos == 'W') graph.first++;
-				else graph.second++;
-			}
-			if(pos == field[i][j+1] && !visited[i][j+1]){ // EAST
-				visited[i][j+1] = 1;
-				flag = 1;
-				if(pos == 'W') graph.first++;
-				else graph.second++;
-			}
-			if(pos == field[i][j-1] && !visited[i][j-1]){ // WEST
-				visited[i][j-1] = 1;
-				flag = 1;
-				if(pos == 'W') graph.first++;
-				else graph.second++;
-			}
-			if(!flag){ // 현재점에서 같은 팀이 근처에 없을 때
-				if(pos == 'W') {
-					result.first += pow(graph.first,2);
-					graph.first = 0;
-				}
-				else{
-					result.second += pow(graph.second,2);
-					graph.second = 0;
-				}
+	for(int i=1; i<height+1; i++){
+		for(int j=1; j<width+1; j++){
+			if(!visited[i][j]){
+				if(field[i][j] == 'W') result.first += bfs(i,j);
+				if(field[i][j] == 'B') result.second += bfs(i,j);
 			}
 		}
 	}
 	return result;
 }
+string getEmptyStr(int size){
+	string empty;
+	for(int i=0; i<size; i++){
+		empty.append("E");
+	}
+	return empty;
+}
+
 int main(int argc, char* argv[]){
 	int width, height;
 	cin >> height >> width;
 	cin.ignore(); // buffer clear
-
+	
+	field.push_back(getEmptyStr(width+2));	
 	for(int i=0; i<height; i++){ // init
 		string line;
 		getline(cin, line);
+		line.insert(0,"E");
+		line.append("E");
 		field.push_back(line);
 	}
+	field.push_back(getEmptyStr(width+2));	
 
 	pair<int, int> result = solution(width, height);
-	cout << result.first << " " << result.second  << '\n';
+	cout << result.first << " " << result.second << '\n';
 	return 0;
 }
