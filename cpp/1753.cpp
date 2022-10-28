1b8ebb115983e1e0ea77af0432534d2ebb8d4845
byoungchan <byoungchan9321@gmail.com>
2022-10-28 20:51:42 +0900

    1753.cpp 힙 이용하여 풀이

--- cpp/1753.cpp
+++ cpp/1753.cpp
@@ -1,60 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 #define INF 99999
 using namespace std;
 
-int getSmallerIdx(int dist[], int vertex, int visited[]){
-	int min = INF;
-	int res = 0;
-	for(int i=1; i<=vertex; i++){
-		if(min > dist[i] && !visited[i]){
-			min = dist[i];
-			res = i;
-		}
-	}
-	return res;
-}
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	int vertex, edge, weigh, start;
+	int vertex, edge, weight, start;
 	cin >> vertex >> edge;	
-	int memory[vertex+1][vertex+1];
-	int visited[vertex+1];
-	fill(&memory[0][0], &memory[vertex+1][vertex+1], INF);
-	fill(&visited[0], &visited[vertex+1], 0);
-
-	int dist[vertex+1];
+	vector<pair<int, int> > memory[vertex+1];
 	cin >> start;
 	for(int i=1; i<=edge; i++){
 		int from, to, weight;
 		cin >> from >> to >> weight;
-		memory[from][from] = 0;
-		memory[to][to] = 0;
-		memory[from][to] = weight;
+		memory[from].push_back(make_pair(to, weight));
 	}
+
+	int d[vertex+1]; 
 	for(int i=1; i<=vertex; i++){
-		dist[i] = memory[start][i]; // 현재정점으로 부터 다른 정점까지의 거리
+		d[i] = INF;
 	}
-	visited[start] = 1;
-	for(int i=1; i<=vertex-1; i++){
-		int current = getSmallerIdx(dist, vertex, visited); // 현재정점으로부터 최소비용을 가지는 정점의 인덱스 이는 곧 다음 방문할 점.
-		visited[current] = 1; // 방문 표시
-		for(int j=1; j<=vertex; j++){ // 방문한 정점에서 모든 정점
-			if(!visited[j]){ // 방문하지 않은 정점들에 한에 
-				if(dist[current] + memory[current][j] < dist[j]){ // 이전 정점의 비용 + 현재 정점에서 j로 가는 비용이 이전정점에서 J로 가는 비용으보다 작다면
-					dist[j] = dist[current] + memory[current][j]; // 최소비용으로 업데이트 한다.
-				}
+	
+	// dijkstra start
+	d[start] = 0;
+	priority_queue<pair<int, int> > pq; // 상태는 최대 힙
+	pq.push(make_pair(start, 0));
+	while(!pq.empty()){
+		int current = pq.top().first; // 현재정점으로부터 최소비용을 가지는 정점의 인덱스 이는 곧 다음 방문할 점.
+		int distance = -pq.top().second;  // 가중치에 -를 붙이면 최대의 값은 가장 하위트리쪽으로 빠지면서 최소 힙으로 변환이 가능하다.
+		pq.pop();
+		if(d[current] < distance) continue;
+		for(int i=0; i < memory[current].size(); i++){ // 현재 정점에서 이동가능한 모든 정점을 검사
+			// 선택된 노드의 인접 노드
+			int next = memory[current][i].first;
+			// 선택된 노드를 거쳐서 인접노드로 가는 비용
+			int nextDistance = distance + memory[current][i].second;
+			if(nextDistance < d[next]){
+				cout << "update" << '\n';
+				d[next] = nextDistance;
+				pq.push(make_pair(next, -nextDistance));
 			}
 		}
 	}
+	// update end 
 	for(int i=1; i<=vertex; i++){
-		if(dist[i] == INF){
+		if(d[i] == INF){
 			cout << "INF" << '\n';
 		}
-		else cout << dist[i] << '\n';
+		else cout << d[i] << '\n';
 	}
 	return 0;
 }
