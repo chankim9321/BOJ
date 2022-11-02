a8bc4e861383715ddff6e1844e9b3d0dcab8a56e
byoungchan <byoungchan9321@gmail.com>
2022-11-03 03:12:26 +0900

    동적할당 오류 수정

--- cpp/12865.cpp
+++ cpp/12865.cpp
@@ -24,11 +24,18 @@ int main(int argc, char* argv[]){
 	// DP 배열의 세로축은 아이템의 수, 가로축은 무게한도를 의미한다.
 	// 정답값은 DP[item][weightLimit] 에 존재한다. 
 	
-	int DP[item+1][weightLimit];
-	fill(DP[0], DP[0]+item*weightLimit, 0);
+	int DP[item+1][weightLimit+1];
+	fill(DP[0], DP[0]+(item+1)*(weightLimit+1), 0);
+	/*
+	for(int i=0; i<=weightLimit; i++){
+		cout << DP[0][i] << " - ";
+	}
+	cout << '\n';
+	*/
 	for(int i=1; i<=item; i++){ // 아이템이 1개 이상인 상태부터 시작
+		//cout << DP[i][0] << " - ";
 		for(int j=1; j<=weightLimit; j++){ // 아이템의 갯수가 정해졌을 떄, 무게한도 1이상 부터 시작
-			int preBag = DP[i-1][j]; // 이전 싸이클의 가방 내용 (물건이 없는 경우)
+			int preBag = DP[i-1][j]; // 이전 싸이클의 가방 내용 (무게가 그대로인 경우)
 			int itemValue = items[i].second;
 			int itemWeight = items[i].first;
 			if(j < itemWeight){ // 현재 무게한도가 새로운 아이템의 무게보다 낮은 경우는 아이템을 넣을 수 없으므로
@@ -39,7 +46,7 @@ int main(int argc, char* argv[]){
 				int newBag = preItem + itemValue;
 				DP[i][j] = max(preBag, newBag);	
 			}
-		//	cout << DP[i][j] << " - ";
+			//cout << DP[i][j] << " - ";
 		}
 		//cout << '\n';
 	}
