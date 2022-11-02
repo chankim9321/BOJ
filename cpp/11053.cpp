9e6b314a0e481455db59dbfdc39936ab4a7f656a
byoungchan <byoungchan9321@gmail.com>
2022-11-02 21:35:43 +0900

    11053.cpp 또 수정

--- cpp/11053.cpp
+++ cpp/11053.cpp
@@ -15,22 +15,15 @@ int main(int argc, char* argv[]){
 	}
 	// solution
 	int *dp = new int(size);
-	int result =0;
 	fill(dp, dp+size, 1);
-	dp[0] = 1;
+	int result = dp[0];
 	for(int i=1; i<size; i++){
-		bool is_changed = false;
-		for(int j=0; j<i; j++){
+		for(int j=0; j<=i; j++){
 			if(arr[i] > arr[j]){
-				dp[i] = max(dp[j], dp[i]);
-				is_changed = true;
+				dp[i] = max(dp[j]+1, dp[i]);
 			}
 		}
-		if(is_changed) dp[i]++;
-	}
-	for(int i=0; i<size; i++){
 		result = max(result, dp[i]);
-		//cout << dp[i] << " - ";
 	}
 	cout << result << '\n';
 	return 0;
