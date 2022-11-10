367f0596548e8733f614c990b7043668f9f28d43
byoungchan <byoungchan9321@gmail.com>
2022-11-10 16:30:52 +0900

    2206.cpp 일부 수정

--- cpp/2206.cpp
+++ cpp/2206.cpp
@@ -3,16 +3,15 @@
 #include <queue>
 using namespace std;
 
-int visited[1003][1003][2];
+int visited[1001][1001][2];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {1, -1, 0, 0};
 int main(int argc, char* argv[]){
 	// init
-	fill_n(&visited[0][0][0], 1003*1003*2, 0);
+	fill_n(&visited[0][0][0], 1001*1001*2, 0);
 	int N, M;
 	scanf("%d %d", &N, &M);
-	int arr[1003][1003];
-	fill_n(&arr[0][0], 1003*1003, -1);
+	int arr[1001][1001];
 	for(int i=1; i<N+1; i++){
 		for(int j=1; j<M+1; j++){
 			scanf("%1d", &arr[i][j]);
@@ -20,7 +19,7 @@ int main(int argc, char* argv[]){
 	}
 	// solution
 	queue<pair<int, pair<int, int> > > q;
-	q.push(make_pair(1, make_pair(1, 0))); // y 좌표 x 좌표 벽을 부쉈는지에 대한 여부
+	q.push(make_pair(1, make_pair(1, 0)));
 	visited[1][1][0] = visited[1][1][1] = 1;
 	arr[1][1] = 1;
 	while(!q.empty()){
@@ -28,26 +27,28 @@ int main(int argc, char* argv[]){
 		int x = q.front().second.first;
 		int y = q.front().first;
 		q.pop();
+		if(y == N && x == M){
+			printf("%d\n", visited[N][M][crashed]);
+			return 0;
+		}
 		for(int i=0; i<4; i++){
 			int next_x = x+dx[i];
 			int next_y = y+dy[i];
-			// 맵상에서 벽, 한번도 부순적이 없으며, 방문하지 않은 곳
-			if(arr[next_y][next_x] == 1 && crashed == 0 && !visited[next_y][next_x][crashed+1]){
-				visited[next_y][next_x][crashed+1] = 1;
-				arr[next_y][next_x] = arr[y][x] + 1;
-				q.push(make_pair(next_y, make_pair(next_x, crashed + 1)));
-			}
-			// 맵상에서 벽이 아니고, 방문하지 않은 곳
-			if(arr[next_y][next_x] == 0 && !visited[next_y][next_x][crashed]){
-				visited[next_y][next_x][crashed] = 1;
-				arr[next_y][next_x] = arr[y][x] + 1;
-				q.push(make_pair(next_y, make_pair(next_x, crashed)));
+			// 변화된 좌표가 인덱스를 벗어나지 않으면서
+			if(next_x > 0 && next_y > 0 && next_x <= M && next_y <= N){
+				// 맵상에서 벽, 한번도 부순적이 없으며, 방문하지 않은 곳
+				if(arr[next_y][next_x] == 1 && crashed == 0 ){
+					visited[next_y][next_x][crashed+1] = visited[y][x][crashed] + 1;
+					q.push(make_pair(next_y, make_pair(next_x, crashed + 1)));
+				}
+				// 맵상에서 벽이 아니고, 방문하지 않은 곳
+				if(arr[next_y][next_x] == 0 && visited[next_y][next_x][crashed] == 0){
+					visited[next_y][next_x][crashed] = visited[y][x][crashed] + 1;
+					q.push(make_pair(next_y, make_pair(next_x, crashed)));
+				}
 			}
 		}
 	}
-	if(N == 1 && M == 1) printf("1\n");
-	else if(arr[N][M] == 0) printf("-1\n");
-	else printf("%d\n", arr[N][M]);
-	
+	printf("-1\n");
 	return 0;
 }
