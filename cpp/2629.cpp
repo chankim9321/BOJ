afed00d3ff9b79f8ed0ade7ef87ca99a388dec66
byoungchan <byoungchan9321@gmail.com>
2022-11-11 17:14:13 +0900

    2629.cpp dp사용

--- cpp/2629.cpp
+++ cpp/2629.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int solution(int items[], int beadWeight, int weightSum, int seq){
-	if(beadWeight == weightSum) return 1;	
-	if(seq == -1) return 0;
-	else{
-		int plusBeadWeight = beadWeight + items[seq]; // 구슬쪽에 무게추를 추가하는 경우
-		int minusBeadWeight = beadWeight - items[seq]; // 무게추를 추가하지 않는 경우( 무게추 쟁반에 올려놓는 경우)
-		weightSum -= items[seq];
-		seq--;
-		// 무게추를 사용해서 구슬 쟁반에 놓는 경우, 무게추쟁반에 놓는 경우, 해당 무게추를 사용하지 않는 경우
-		return max(solution(items, plusBeadWeight, weightSum, seq), max(solution(items, minusBeadWeight, weightSum, seq), solution(items,beadWeight,weightSum,seq)));
-	}
+int items[30];
+int dp[31][15001]; // 추의 갯수와 모든 추를 이용한 나올수 있는 조합의 최대
+int weight;
+// beadWeight 를 0으로 만드는게 목적.
+// 즉, 추의 갯수를 변화시키면서 beadWeight 가 0인 값이 true가 되면 목적 달성.
+void sol(int seq, int beadWeight){
+	if(seq>weight || dp[seq][weight] == 1) return;	
+	dp[seq][beadWeight] = 1;
+	sol(seq+1, beadWeight + items[seq]); // 구슬 쟁반에 추를 추가하는 경우
+	sol(seq+1, abs(beadWeight - items[seq])); // 추 쟁반에 추를 추가하는 경우 --> 구슬 쟁반의 무게가 줄어든걸로 간주
+	sol(seq+1, beadWeight); // n번째에 있는 추를 버리는 경우
 }
 int main(int argc, char* argv[]){
 	// fast I/O
@@ -19,20 +20,19 @@ int main(int argc, char* argv[]){
 	cin.tie(0);
 	cout.tie(0);
 
-	int weight;
 	cin >> weight;
-	int items[weight];
 	int weightSum = 0; // 무게추의 합
 	for(int i=0; i<weight; i++){
 		cin >> items[i];
 		weightSum += items[i];
 	}
+	sol(0,0);
 	int beads;
 	cin >> beads;
 	for(int i=0; i<beads; i++){
 		int beadWeight;
 		cin >> beadWeight;
-		if(solution(items, beadWeight, weightSum, weight-1) == 1){
+		if(dp[weight][beadWeight] == 1){
 			cout << "Y ";
 		}
 		else{
