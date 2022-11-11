570509fa601bb3c984ab0eed1c177710654a255d
byoungchan <byoungchan9321@gmail.com>
2022-11-11 21:54:38 +0900

    20006.cpp 코드 최적화

--- cpp/20006.cpp
+++ cpp/20006.cpp
@@ -13,16 +13,9 @@ int main(int argc, char* argv[]){
 	cin >> players >> rooms;
 	list<pair<vector<pair<int, char> >, int > > roomList; // ( 방의 인원, 방 주인 레벨) 을 리스트로 저장
 
-	// 첫번째 방 생성
-	int l;
-	char n;
-	cin >> l >> n;
-	vector<pair<int, char> > v;
-	v.push_back({l, n});
-	roomList.push_back({v,l});
 	list<pair<vector<pair<int, char> >, int> > ::iterator iter;
-	for(int i=1; i<players; i++){
-		bool is_success = false;
+	for(int i=0; i<players; i++){
+		bool is_entered = false;
 		int level;
 		char name;
 		cin >> level >> name;
@@ -31,11 +24,11 @@ int main(int argc, char* argv[]){
 			int roomPlayerSize = iter->first.size();
 			if(levelRange + 10 >= level && levelRange - 10 <= level && roomPlayerSize < rooms){ // 입력받은 레벨내 허용되는 방이 존재한다면
 				iter->first.push_back({level, name});	
-				is_success = true;
+				is_entered = true; // 방에 들어갔음을 확인 
 				break;
 			}
 		}
-		if(!is_success){
+		if(!is_entered){ // 모든방을 뒤져서 들어가지 못했다면
 			vector<pair<int, char> > nv; // 방 생성
 			nv.push_back({level, name}); // 방 인원 추가
 			roomList.push_back({nv, level}); // 생성된 방과 적정레벨을 방 목록에 추가
@@ -45,18 +38,15 @@ int main(int argc, char* argv[]){
 	for(iter = roomList.begin(); iter != roomList.end(); iter++){
 		int roomPlayerSize = iter->first.size();
 		vector<pair<int, char> > roomInfo(iter->first);
-		sort(roomInfo.begin(), roomInfo.end(), sortByName);
-		if(roomPlayerSize < 5){
+		sort(roomInfo.begin(), roomInfo.end(), sortByName); // 이름순으로 정렬
+		if(roomPlayerSize < rooms){
 			cout << "Waiting!\n";
-			for(int j=0; j<roomPlayerSize; j++){
-				cout << roomInfo[j].first << " " << roomInfo[j].second << '\n';
-			}
 		}
 		else{
 			cout << "Started!\n";
-			for(int j=0; j<roomPlayerSize; j++){
-				cout << roomInfo[j].first << " " << roomInfo[j].second << '\n';
-			}
+		}
+		for(int j=0; j<roomPlayerSize; j++){
+			cout << roomInfo[j].first << " " << roomInfo[j].second << '\n';
 		}
 	}
 	
