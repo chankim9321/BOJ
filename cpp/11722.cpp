1c01e6f9cc3b0d69b466344f447dbef562285ab3
byoungchan <byoungchan9321@gmail.com>
2022-11-12 21:26:28 +0900

    11722.cpp 역으로 재배치x

--- cpp/11722.cpp
+++ cpp/11722.cpp
@@ -15,17 +15,12 @@ int main(int argc, char* argv[]){
 		cin >> arr[i];	
 	}
 	// sol
-	int rsArr[size];
-	for(int i=0; i<size; i++){
-		rsArr[i] = arr[size-1-i];
-	}
 	int dp[size];
-	fill_n(&dp[0], size, 0);
-	dp[0] = 1;
+	fill_n(&dp[0], size, 1);
 	int res = dp[0];
 	for(int i=1; i<size; i++){
 		for(int j=0; j<i; j++){
-			if(rsArr[i] > rsArr[j]){
+			if(arr[i] < arr[j]){
 				dp[i] = max(dp[i], dp[j] +1);
 			}
 		}
