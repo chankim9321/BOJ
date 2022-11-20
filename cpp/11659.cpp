7fbd1a00bfabdf805e7874f6ffae643e56081a89
byoungchan <byoungchan9321@gmail.com>
2022-11-20 21:08:07 +0900

    11659.cpp O(n)H

--- cpp/11659.cpp
+++ cpp/11659.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int numbers, sums;
 
-void sol(int arr[], pair<int, int> range){
-	int sum = 0;
-	for(int i=1; i<=numbers; i++){
-		if(i >= range.first && i <= range.second){
-			sum += arr[i];
-		}
-	}
-	cout << sum << '\n';
-}
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+	int numbers, sums;
 	cin >> numbers >> sums;
 	int arr[numbers+1];
+	arr[0] = 0;
 	for(int i=1; i<=numbers; i++){
 		cin >> arr[i];
+	}
+	for(int i=2; i<=numbers; i++){ // init prefix sum
+		arr[i] += arr[i-1];
 	}
 	for(int i=0; i<sums; i++){
 		pair<int, int> range;
 		cin >> range.first >> range.second;
-		sol(arr, range);
+		cout << arr[range.second] - arr[range.first-1] << '\n';
 	}
 	return 0;
 }
