9e470346cbc052fdd49777c726f2c0f0022134c6
byoungchan <byoungchan9321@gmail.com>
2022-11-24 19:11:38 +0900

    13913.cpp 무한루프 미해결

--- cpp/13913.cpp
+++ cpp/13913.cpp
@@ -2,39 +2,34 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
+#include <stack>
 using namespace std;
 
 int cnt[100001];
 int next_x[3] ={-1,1,2};
 bool visitedPos[100001] = { false, };
-vector<int> road(100000);
+int roadTracker[100001] = {0,};
 
 void sol(int subinPos, int brotherPos){
-	queue<pair<pair<int, int>, vector<int>>> q;
-	vector<int> v;
-	q.push({{subinPos, 0}, v});
+	queue<pair<int, int>> q;
+	q.push({subinPos, 0});
 	while(!q.empty()){
-		pair<int, int> present = q.front().first;
-		vector<int> visited = q.front().second;
+		pair<int, int> present = q.front();
 		q.pop();
 		cnt[present.first] = min(cnt[present.first], present.second);
-		visited.push_back(present.first);
-		visitedPos[present.first] = true;
-		if(present.first == brotherPos){ // 현재 위치가 동생이 있는 위치라면 지나온 길을 가장 짧은 것으로 업데이트
-			if(road.size() > visited.size()){
-				road = visited;
-			}
-		}
+		visitedPos[present.first] = true;	
 		for(int i=0; i<3; i++){
 			int nextPos;
 			if(i==2){
 				nextPos = present.first * next_x[i];
+				if(nextPos < 100001) roadTracker[nextPos] = present.first;
 			}
 			else{
 				nextPos = present.first + next_x[i];
+				if(nextPos < 100001) roadTracker[nextPos] = present.first;
 			}
 			if(nextPos < 100001 && !visitedPos[nextPos]){
-				q.push({{nextPos, present.second+1}, visited});
+				q.push({nextPos, present.second+1});
 			}
 		}
 	}
@@ -58,10 +53,21 @@ int main(int argc, char* argv[]){
 	else {
 		sol(subinPos, brotherPos);
 		cout << cnt[brotherPos] << '\n';
-		for(int i=0; i<road.size(); i++){
-			cout << road[i] << " ";
+
+		stack<int> road;
+		int k=brotherPos;
+		road.push(k); // 동생위치
+		while(k != subinPos){
+			road.push(roadTracker[k]);
+			k = roadTracker[k];
+		}
+		road.push(k); // 수빈 위치
+		while(!road.empty()){
+			cout << road.top() << " ";
+			road.pop();
 		}
 		cout << '\n';
+
 		return 0;
 	}
 }
