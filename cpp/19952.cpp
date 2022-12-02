6e2ae5974d66b307362d9acce9cc568a0a8d24dd
byoungchan <byoungchan9321@gmail.com>
2022-12-02 22:04:38 +0900

    19952.cpp 수정

--- cpp/19952.cpp
+++ cpp/19952.cpp
@@ -13,7 +13,6 @@ int visited[101][101];
 
 bool bfs(){
 	queue<tuple<int, int, int>> q; // 좌표값과 남은 힘
-	//int nextL = map[start.first][start.second]; // 스타트 지점의 높이
 	q.push(make_tuple(start_y, start_x, strength)); // 시작점
 	visited[start_y][start_x] = strength;
 	while(!q.empty()){
@@ -23,18 +22,20 @@ bool bfs(){
 		int x = get<1>(present);
 		int str = get<2>(present);
 		int l = map[y][x]; // 현재칸 높이
-		visited[y][x] = str;
+		if(str == 0) break; // 힘이 다하면 앞으로 나아가지 못한다.
 		for(int i=0; i<4; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			int nextL = map[ny][nx]; // 다음칸 높이
+			int nextL = map[ny][nx]; // 장애물 높이
 			if(ny > 0 && nx > 0 && ny <= height && nx <= width){
 				if(visited[ny][nx] < str-1){
-					if(nextL <= l) { // 다음 칸의 높이가 현재 높이보다 낮거나 같은 경우
+					if(nextL <= l) { // 다음 장애물의 높이가 현재 높이보다 낮거나 같은 경우
+						visited[ny][nx] = str-1;
 						q.push(make_tuple(ny,nx,str-1));	
 					}
-					else{ // 다음 칸의 높이가 큰 경우
+					else{ // 다음 장애물의 높이가 큰 경우
 						if((nextL - l) <= str){ // 점프해야 할 높이보다 힘이 많다면 이동가능.
+							visited[ny][nx] = str-1;
 							q.push(make_tuple(ny,nx,str-1));
 						}
 					}
