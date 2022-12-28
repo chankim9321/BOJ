f66038a4e00a55d28c970a08a7046d10daad4b9d
byoungchan <byoungchan9321@gmail.com>
2022-12-29 01:36:07 +0900

    7662.cpp 오류 해결2

--- cpp/7662.cpp
+++ cpp/7662.cpp
@@ -26,6 +26,7 @@ int main(int argc, char* argv[]){
 				if(select == 1){ // 최대힙에서 제거	
 					while(!m[maxHeap.top()] && !maxHeap.empty()){ // 이미 최소힙에서 삭제된 값이라면
 						maxHeap.pop(); // 제거
+						if(maxHeap.empty()) break;
 					}
 					if(!maxHeap.empty()){
 						m[maxHeap.top()] = false; // 제거한 값은 항상 false
@@ -35,6 +36,7 @@ int main(int argc, char* argv[]){
 				else if(select == -1){ // 최소힙에서 제거
 					while(!m[-minHeap.top()] && !minHeap.empty()){
 						minHeap.pop();
+						if(minHeap.empty()) break;
 					}
 					if(!minHeap.empty()){
 						m[-minHeap.top()] = false;
@@ -57,9 +59,11 @@ int main(int argc, char* argv[]){
 		}
 		while(!m[maxHeap.top()] && !maxHeap.empty()){
 			maxHeap.pop();
+			if(maxHeap.empty()) break;
 		}
 		while(!m[-minHeap.top()] && !minHeap.empty()){
 			minHeap.pop();
+			if(minHeap.empty()) break;
 		}
 		if(!maxHeap.empty() && !minHeap.empty()){
 			cout << maxHeap.top() << " " << -minHeap.top() << '\n';
