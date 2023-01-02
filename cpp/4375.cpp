df4fc4cca8f8556fe797c29f53993d33b4e85c85
byoungchan <byoungchan9321@gmail.com>
2023-01-02 20:23:41 +0900

    4375.cpp 모듈러 연산 법칙

--- cpp/4375.cpp
+++ cpp/4375.cpp
@@ -7,17 +7,21 @@ int main(int argc, char* argv[]){
 	while(!cin.eof()){
 		int n;
 		cin >> n;
-
+		if(n == 1) {
+			cout << 1 << '\n';
+			continue;
+		}
 		int cipher = 2;
 		int answer;
-		long long k = 1;
+		unsigned long long int k = 1;
 		while(1){
 			k =	k*10 +1; 	 
 			if(k % n == 0){
 				answer = cipher;
 				break;
 			}
-			else{
+			else{ // n으로 모듈러 연산이 이루어 지지 않을 때
+				k = k%n;
 				cipher++;			
 			}
 		}
