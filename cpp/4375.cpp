b7c231fea1d8473895fa895f5d2da8382fa3e90d
byoungchan <byoungchan9321@gmail.com>
2023-01-02 19:43:23 +0900

    4375.cpp 풀이

---
+++ cpp/4375.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){
+	int n;
+	cin >> n;
+
+	int cipher = 2;
+	int answer;
+	long long k = 1;
+	while(1){
+		k =	k*10 +1; 	 
+		if(k % n == 0){
+			answer = cipher;
+			break;
+		}
+		else{
+			cipher++;			
+		}
+	}
+	cout << answer << '\n';
+	return 0;
+}
