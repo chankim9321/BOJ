eda87562186815450b30513ec444a65d4d99d9bb
byoungchan <byoungchan9321@gmail.com>
2023-02-09 12:54:57 +0900

    14621.cpp 방문 배열 추가

--- cpp/14621.cpp
+++ cpp/14621.cpp
@@ -7,10 +7,12 @@ int vert, edge;
 vector<char> univState;
 vector<pair<int, pair<int, int>>> v;
 vector<int> parent;
+bool visited[1001];
 void init(){
 	cin >> vert >> edge;	
 	univState.resize(vert+1);
 	parent.resize(vert+1);
+	fill_n(&visited[0], 1001, false);
 	for(int i=1; i<=vert; i++){
 		cin >> univState[i];
 	}
@@ -49,10 +51,17 @@ void kruskal(){
 		if(!hasCycle(from, to) && univState[from] != univState[to]){ // 사이클 형성을 이루지않고 서로가 다른 집합일 때
 			result += weight;
 			setUnion(from, to);
+			visited[from] = true;
+			visited[to] = true;
 		}
 	}
-	if(result == 0) cout << -1 << '\n';
-	else cout << result << '\n';
+	for(int i=1; i<=vert; i++){
+		if(!visited[i]) {
+			cout << -1 << '\n';
+			return;
+		}
+	}
+	cout << result << '\n';
 }
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false);
