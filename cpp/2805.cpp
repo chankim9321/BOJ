f57ca540e9f20e39ac51c69c53380ab866c578f6
byoungchan <byoungchan9321@gmail.com>
2023-03-03 15:57:25 +0900

    2805.cpp 수정

--- cpp/2805.cpp
+++ cpp/2805.cpp
@@ -17,8 +17,8 @@ void init(){
 	}
 }
 void sol(){
-	ll answer = MAX_HEIGHT;
-	while(minHeight < maxHeight){
+	ll result = MAX_HEIGHT;
+	while(minHeight <= maxHeight){
 		ll sawHeight = (minHeight + maxHeight)/2;
 		ll timbers = 0;
 		for(int i=0; i<trees.size(); i++){
@@ -28,17 +28,17 @@ void sol(){
 		}
 		if(timbers > m){ // 자른 목재가 요구사항보다 많을 때
 			minHeight = sawHeight + 1;
-			answer = min(sawHeight, answer);
+			result = sawHeight;
 		}
 		else if(timbers < m){
-			maxHeight = sawHeight;
+			maxHeight = sawHeight - 1;
 		}
 		else{
-			answer = sawHeight;
+			result = sawHeight;
 			break;
 		}
 	}
-	cout << answer << '\n';
+	cout << result << '\n';
 }
 int main(int argc, char* argv[]){
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
