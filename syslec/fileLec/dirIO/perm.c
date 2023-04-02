50ce6a046a3b4f27e0e970eef9943850c8a8940b
byoungchan <byoungchan9321@gmail.com>
2023-04-02 17:14:07 +0900

    소스 추가 및 이름 변경

--- syslec/fileLec/dirIO/perm.c
+++ syslec/fileLec/dirIO/perm.c
@@ -7,14 +7,35 @@ int main(int argc, char* argv[]){
 	char* filename = argv[1];
 	struct stat buf;
 	stat(filename, &buf);	
+	printf("------------ Read permission ----------\n");
+	if((buf.st_mode & 00400) != 0){
+		printf("%s : user has a read permission\n", filename);
+	}
+	if((buf.st_mode & (00400 >> 3)) != 0){
+		printf("%s : group has a read permission\n", filename);
+	}
+	if((buf.st_mode & (00400 >> 3)) != 0){
+		printf("%s : other has a read permission\n", filename);
+	}
+	printf("------------ Write permission ----------\n");
+	if((buf.st_mode & 00200) != 0){
+		printf("%s : user has a write permission\n", filename);
+	}
+	if((buf.st_mode & (00200 >> 3)) != 0){
+		printf("%s : group has a write permission\n", filename);
+	}
+	if((buf.st_mode & (00200 >> 3)) != 0){
+		printf("%s : other has a write permission\n", filename);
+	}
+	printf("------------ Excecution permission ----------\n");
 	if((buf.st_mode & 00100) != 0){
-		printf("%s : user has a execution permission\n", filename);
+		printf("%s : user has a excecution permission\n", filename);
 	}
 	if((buf.st_mode & (00100 >> 3)) != 0){
-		printf("%s : group has a execution permission\n", filename);
+		printf("%s : group has a excecution permission\n", filename);
 	}
 	if((buf.st_mode & (00100 >> 3)) != 0){
-		printf("%s : other has a execution permission\n", filename);
+		printf("%s : other has a excecution permission\n", filename);
 	}
 	return 0;
 }
