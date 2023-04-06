174679276bfb1b4bad1ebdb4c3f142174f82d076
byoungchan <byoungchan9321@gmail.com>
2023-04-06 18:30:47 +0900

    2470.cpp 투 포인터 설정을 앞 뒤로

--- BOJ/cpp/2470.cpp
+++ BOJ/cpp/2470.cpp
@@ -14,27 +14,46 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
 
+	bool plus = false, minus = false;
 	cin >> n;
 	for(int i=0; i<n; i++){
 		int num;
 		cin >> num;
 		v.push_back(num);
+		if(num > 0) plus = true;
+		if(num < 0) minus = true;
 	}
 	sort(v.begin(), v.end());
-
-	int left = 0, right = 1;
-	while(left < right){
-		int res = abs(v[left] + v[right]);
-		if(differ > res){
-			differ = res;
-			ans.first = v[left];
-			ans.second = v[right];
-			right++;
+	if(plus && !minus){
+		ans.first = v[0];
+		ans.second = v[1];
+	}
+	else if(!plus && minus){
+		ans.first = v[n-2];
+		ans.second = v[n-1];
+	}
+	else{
+		int left = 0;
+		int right = n-1;
+		while(left < right){
+			int res = v[left] + v[right];
+			if(differ > abs(res)) {
+				ans.first = v[left];
+				ans.second = v[right];
+				differ = abs(res);
+			}
+			if(res < 0){
+				left++;
+			}
+			else if(res > 0){
+				right--;
+			}
+			else{
+				ans.first = v[left];
+				ans.second = v[right];
+				break;
+			}
 		}
-		else{
-			left++;
-		}
-		if(right == n) break;
 	}
 	cout << ans.first << " " << ans.second << '\n';
 	return 0;
