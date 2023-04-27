582610d908b88a36c5f534a6e2a5bd5bdda1028c
byoungchan <byoungchan9321@gmail.com>
2023-04-27 18:48:14 +0900

    1865 수정-1

--- BOJ/cpp/1865.cpp
+++ BOJ/cpp/1865.cpp
@@ -8,23 +8,24 @@ int n,m,w;
 bool BellmanFord(vector<vector<pair<int, int>>>& edge, int start){
 	vector<int> dist(n+1, 10e8);
 	dist[start] = 0;
-	int tmp;
 	for(int i=1; i<=n; i++){ // 모든 노드
-		for(int j=0; j<edge[i].size(); j++){ // 모든 간선
-			int current = i;
-			int next = edge[i][j].first;
-			int weight = edge[i][j].second;
-			if(dist[current] == 10e8) continue;
-			if(dist[next] > dist[current] + weight) dist[next] = dist[current] + weight;
+		int current = i;
+		for(int j=0; j<edge[current].size(); j++){ // 모든 간선
+			int next = edge[current][j].first;
+			int weight = edge[current][j].second;
+			if(dist[next] > dist[current] + weight){
+				dist[next] = dist[current] + weight;
+			}
 		}
 	}
 	for(int i=1; i<=n; i++){ // 모든 노드
-		for(int j=0; j<edge[i].size(); j++){ // 모든 간선
-			int current = i;
-			int next = edge[i][j].first;
-			int weight = edge[i][j].second;
-			if(dist[current] == 10e8) continue;
-			if(dist[next] > dist[current] + weight) return true;
+		int current = i;
+		for(int j=0; j<edge[current].size(); j++){ // 모든 간선
+			int next = edge[current][j].first;
+			int weight = edge[current][j].second;
+			if(dist[next] > dist[current] + weight) {
+				return true;
+			}
 		}
 	}
 	return false;
@@ -44,7 +45,7 @@ void sol(){
 		for(int j=0; j<w; j++){
 			int from, to, weight;
 			cin >> from >> to >> weight;
-			edge[from].push_back({to, -weight});
+			edge[from].push_back({to, -1 * weight});
 		}
 		if(BellmanFord(edge, 1)) cout << "YES" << '\n'; // 음수 사이클이 존재한다면
 		else cout << "NO" << '\n';
